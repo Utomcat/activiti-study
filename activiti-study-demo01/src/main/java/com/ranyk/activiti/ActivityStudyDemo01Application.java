7c291d2080d743ef8b3cc7a2829d01d644a2b81d
ranyk <1390851149@qq.com>
2020-07-29 13:59:24 +0800

    初始化工作流学习项目

---
+++ activiti-study-demo01/src/main/java/com/ranyk/activiti/ActivityStudyDemo01Application.java
@@ -0,0 +1,16 @@
+package com.ranyk.activiti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ranyi
+ */
+@SpringBootApplication
+public class ActivityStudyDemo01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivityStudyDemo01Application.class, args);
+    }
+
+}
